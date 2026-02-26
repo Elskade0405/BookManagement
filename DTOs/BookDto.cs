9c58faa5b15bd78738dcd3b91961dcadb821c508
Elskade0405 <elskade040501@gmail.com>
2026-02-27 00:27:21 +0700

    done books endpoints

--- DTOs/BookDto.cs
+++ DTOs/BookDto.cs
@@ -4,8 +4,8 @@ namespace BookManagement.DTOs;
 
 public record BookDto
 {
-    [Required] public int Id { get; set; }
-    [Required] public string Name { get; set; }
-    [Required] public string Author { get; set; }
-    [Required] public string Publisher { get; set; }
+    [Required] public required int Id { get; set; }
+    [Required] public required string Name { get; set; }
+    [Required] public required string Author { get; set; }
+    [Required] public required string Publisher { get; set; }
 }
